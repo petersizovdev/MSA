ea3b545ca488f9bfc1b15e4c148e88e537bd0c46
pitersizovdev <pitersizov@gmail.com>
2024-05-02 23:52:38 +0300

    061

---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,12 @@
+import styles from "../Education/education.module.css";
+
+const Education = () => {
+  return (
+    <div className={styles.education}>
+      <div className={styles.leftColumn}></div>;
+      <div className={styles.rightColumn}></div>;
+    </div>
+  );
+};
+
+export default Education;
